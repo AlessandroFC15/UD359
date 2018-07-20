7f80cec3edf2acd1873a731eb6cfa1d67d527e73
Alessandro <alessandro@acception.com.br>
2018-07-20 11:04:26 -0300

    Using Plotly to plot a grouped bar chart of the admission rate per department

--- src/class_1/simpsons_paradox.py
+++ src/class_1/simpsons_paradox.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import pprint
+import plotly.offline
+import plotly.graph_objs as go
 
 df = pd.read_csv('Berkeley.csv')
 
@@ -25,8 +27,33 @@ for dept in list_departments:
         'female_percentage_admission': (number_females_admitted / (number_females_admitted + number_females_rejected)) * 100,
     }
 
-
-
 pp = pprint.PrettyPrinter(indent=4)
 
 pp.pprint(percentage_admission_per_department)
+
+# list_departments = sorted(percentage_admission_per_department.keys())
+
+women_admissions_per_department = [percentage_admission_per_department[dept]['female_percentage_admission'] for dept in list_departments]
+men_admissions_per_department = [percentage_admission_per_department[dept]['male_percentage_admission'] for dept in list_departments]
+
+trace1 = go.Bar(
+    x=list_departments,
+    y=men_admissions_per_department,
+    name='Men',
+    marker=dict(color='rgb(38, 144, 201)')
+)
+trace2 = go.Bar(
+    x=list_departments,
+    y=women_admissions_per_department,
+    name='Women',
+    marker=dict(color='rgb(255, 150, 232)')
+)
+
+data = [trace1, trace2]
+layout = go.Layout(
+    title='Admission Percentage per Department',
+    barmode='group'
+)
+
+fig = go.Figure(data=data, layout=layout)
+plotly.offline.plot(fig, filename='grouped-bar')
